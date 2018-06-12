ae936f106b28c63d0598dd1bcd720676c9ae39f6
Mariia_Ganza <mariia_ganza@epam.com>
2018-06-12 19:59:23 +0300

    testing deployment

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var express = require('express')
+var express = require('express');
 var app = express();
+
 app.get('/', (req, res) => {
     res.send({
-        hi: 'I\'m cat!'
+        hi: 'buddy'
     })
 });
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT);
