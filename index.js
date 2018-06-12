573729186379bb09b9ce176e14061856883ae5d6
Mariia_Ganza <mariia_ganza@epam.com>
2018-06-12 19:37:39 +0300

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var app = express();
+app.get('/', (req, res) => {
+    res.send({
+        hi: 'I\'m cat!'
+    })
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
