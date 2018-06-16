d96da81bdc0fc7b87a9cfa25e6ef5eba7a842732
Mariia_Ganza <mariia_ganza@epam.com>
2018-06-17 01:07:21 +0300

    mongoDB first working version
    Now we can insert user's info in MongoDB.

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     googleClientID: '988342111146-5oe57sl61n86s3da65jvffr6h2qgrpri.apps.googleusercontent.com',
     googleClientSecret: '2lhspS5OrXZ1XrKdS136B6Gj',
-    mangoURI: 'mongodb://admin:25carpediem64@ds161710.mlab.com:61710/so-triky-server-dev'
+    mangoURI: 'mongodb://superman:password123@ds163870.mlab.com:63870/best_server_ever'
 };
